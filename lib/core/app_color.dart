5608905e30c734835b3675ee4d4445580a6001fc
hussein_salah <husseinsalah2528@gmail.com>
2024-02-16 15:17:52 +0200

    add defult color for app

---
+++ lib/core/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color mainColor = const Color(0xff197B8D);
+  static Color primaryColor = const Color(0xff004854);
+  static Color backgroundColor = const Color(0xffEFF1F1);
+  static const BoxShadow defaultBoxShadow = BoxShadow(
+    color: Color.fromRGBO(0, 0, 0, 0.25),
+    offset: Offset(0, 4),
+    blurRadius: 4,
+    spreadRadius: 0,
+  );
+}
