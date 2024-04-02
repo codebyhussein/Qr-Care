7dc3f343d2381b3983aa6547fb1c69a93adfa268
hussein_salah <husseinsalah2528@gmail.com>
2024-04-02 17:17:23 +0200

    Note: The commented out http request in main.dart is for testing purposes and is not part of the changes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:qr_care/app.dart';
 
-void main() {
+import 'package:http/http.dart' as http;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // var url = Uri.https('10.0.2.2:8000', '/grd/auth/login.php');
+  // var response = await http.post(url,
+  //     body: {'account_id': 'doctor_6606de58.com', 'password': '1234'});
+  // print('Response status: ${response.statusCode}');
+  // print('Response body: ${response.body}');
+
   runApp(const QrCare());
 }
