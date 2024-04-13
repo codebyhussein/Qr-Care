ff574666ae0ef9938241b2818311e619e916a81f
hussein_salah <husseinsalah2528@gmail.com>
2024-04-13 18:10:13 +0200

    Update dependencies and fix emergency contact screen
    * Upgrade leak\_tracker to 9.0.13 and leak\_tracker\_testing to 1.0.5
    * Update leak\_tracker\_flutter\_testing

--- lib/features/register/presentation/screens/Screen4.dart
+++ lib/features/register/presentation/screens/Screen4.dart
@@ -46,8 +46,7 @@ class _Screen2State extends State<Screen4> {
               ),
               Center(
                 child: AppWidgets.defultdesrptionRegister(
-                    text:
-                    getTranslated("descEmergencyContact", context)!),
+                    text: getTranslated("descEmergencyContact", context)!),
               ),
               SizedBox(
                 height: 50.h,
@@ -57,34 +56,39 @@ class _Screen2State extends State<Screen4> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    AppWidgets.defultText(text:  getTranslated("contact", context)!),
+                    AppWidgets.defultText(
+                        text: getTranslated("contact", context)!),
                     SizedBox(
                       height: 6.h,
                     ),
                     CustomTextFormField(
                         regEx: AppConst.phonePattern,
                         prefixIcon: const Icon(IconlyLight.call),
-                        hintText:  getTranslated("phone", context)!,
-                        errorMassage: getTranslated("errorMessagePhone", context)!,
+                        hintText: getTranslated("phone", context)!,
+                        errorMassage:
+                            getTranslated("errorMessagePhone", context)!,
                         nameofController: phoneController,
                         keyBoredType: TextInputType.number,
                         isvisble: false),
-                    AppWidgets.defultText(text: getTranslated("emergencyContact", context)! ),
+                    AppWidgets.defultText(
+                        text: getTranslated("emergencyContact", context)!),
                     SizedBox(
                       height: 6.h,
                     ),
                     CustomTextFormField(
                         regEx: r'^[A-Za-z]+$',
                         prefixIcon: const Icon(IconlyLight.profile),
-                        hintText:  getTranslated("name", context)! ,
-                        errorMassage:  getTranslated("errorMessageName", context)! ,
+                        hintText: getTranslated("name", context)!,
+                        errorMassage:
+                            getTranslated("errorMessageName", context)!,
                         nameofController: nameController,
                         keyBoredType: TextInputType.text,
                         isvisble: false),
                     SizedBox(
                       height: 10.h,
                     ),
-                    AppWidgets.defultText(text: getTranslated("mobile", context)!),
+                    AppWidgets.defultText(
+                        text: getTranslated("mobile", context)!),
                     SizedBox(
                       height: 10.h,
                     ),
@@ -93,13 +97,13 @@ class _Screen2State extends State<Screen4> {
                       prefixIcon: const Icon(IconlyLight.call),
                       hintText: getTranslated("phone", context)!,
                       errorMassage:
-                      getTranslated("errorMessageEmergency", context)!,
+                          getTranslated("errorMessageEmergency", context)!,
                       nameofController: MobileController,
                       keyBoredType: TextInputType.number,
                       isvisble: false,
                     ),
                     SizedBox(
-                      height: 70.h,
+                      height: 56.h,
                     ),
                   ],
                 ),
