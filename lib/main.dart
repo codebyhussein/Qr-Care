a233bfed06cd6904425f18300032a7f8cd1da89f
hussein_salah <husseinsalah2528@gmail.com>
2024-04-14 00:07:57 +0200

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:qr_care/app.dart';
 import 'package:qr_care/core/Services/LocalService/Cache_Helper.dart';
 
- 
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  var url = Uri.parse('http://10.0.2.2/grd/auth/login.php');
-  var response = await http.post(url,
-      body: {'account_id': 'doctor_6606de58.com', 'password': '1234'});
-  print('Response status: ${response.statusCode}');
-  print('Response body: ${response.body}');
-
- 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
   await CacheHelper.init();
- 
+
+  // var url = Uri.parse('http://10.0.2.2/grd/auth/login.php');
+  // var response = await http.post(url,
+  //     body: {'account_id': 'doctor_6606de58.com', 'password': '1234'});
+  // print('Response status: ${response.statusCode}');
+  // print('Response body: ${response.body}');
+
   runApp(const QrCare());
 }
