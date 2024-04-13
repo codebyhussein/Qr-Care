c7e6b7a2b32c2bc2012eb5acd87c94235c255072
hussein_salah <husseinsalah2528@gmail.com>
2024-04-13 17:54:46 +0200

    test api

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:qr_care/app.dart';
 
@@ -7,11 +5,12 @@ import 'package:http/http.dart' as http;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // var url = Uri.https('10.0.2.2:8000', '/grd/auth/login.php');
-  // var response = await http.post(url,
-  //     body: {'account_id': 'doctor_6606de58.com', 'password': '1234'});
-  // print('Response status: ${response.statusCode}');
-  // print('Response body: ${response.body}');
+
+  var url = Uri.parse('http://10.0.2.2/grd/auth/login.php');
+  var response = await http.post(url,
+      body: {'account_id': 'doctor_6606de58.com', 'password': '1234'});
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
 
   runApp(const QrCare());
 }
