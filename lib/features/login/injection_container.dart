5d3d2e73a87a5b17c3cf507db2cce7a6d129655e
hussein_salah <husseinsalah2528@gmail.com>
2024-04-14 15:32:09 +0200

    Update login functionality with new API endpoint and add BLoC for state management
    This commit includes the following changes:
    
    1. Modify `LoginRemoteDataSource` to use a new API endpoint for authentication.
    2. Implement `Login

---
+++ lib/features/login/injection_container.dart
@@ -0,0 +1,39 @@
+import 'package:get_it/get_it.dart';
+import 'package:qr_care/features/login/data/datasources/login_local_data_source.dart';
+import 'package:qr_care/features/login/data/datasources/login_remote_data_source.dart';
+import 'package:qr_care/features/login/data/repositories/login_repository_impl.dart';
+import 'package:qr_care/features/login/domain/repositories/login_repository.dart';
+import 'package:qr_care/features/login/domain/usecases/login_use_case.dart';
+import 'package:qr_care/features/login/presentation/cubit/login_cubit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:http/http.dart' as http;
+
+final getIt = GetIt.instance;
+
+void setup() async {
+  // register cubit
+  getIt.registerFactory(() => LoginCubit(loginUseCase: getIt()));
+
+  // usecase
+  getIt.registerLazySingleton(() => LoginUseCase(loginRepository: getIt()));
+
+// Repositoy
+  getIt.registerLazySingleton<LoginRepository>(() =>
+      LoginRepositoryImpl(remoteDataSource: getIt(), localDataSource: getIt()));
+
+// Data Source
+
+  getIt.registerLazySingleton<LoginLocalDataSource>(
+      () => LoginLocalDataSource());
+
+  getIt.registerLazySingleton<LoginRemoteDataSource>(
+      () => LoginRemoteDataSourceImpl(client: getIt()));
+
+//  core for app
+
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  getIt.registerLazySingleton(() => sharedPreferences);
+  getIt.registerLazySingleton(() => http.Client());
+}
