818a03a6eb0ef5170ed3e05d3e0ebdb1f6f67571
hussein_salah <husseinsalah2528@gmail.com>
2024-04-21 20:54:00 +0200

    Implemented ModalProgressHUD in various screens for displaying loading indicators

--- lib/features/forgetPassword/presentation/screens/change_password.dart
+++ lib/features/forgetPassword/presentation/screens/change_password.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iconly/iconly.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:qr_care/config/Localization/Constraine.dart';
 import 'package:qr_care/config/routes/app_routes.dart';
 import 'package:qr_care/core/app_color.dart';
@@ -27,6 +28,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
   TextEditingController newPasswordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -34,108 +36,107 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
       backgroundColor: AppColors.backgroundColor,
       body: BlocConsumer<ResetpasswordCubit, ResetpasswordState>(
         listener: (context, state) {
+          if (state is ResetpasswordLoading) {
+            isLoading = true;
+          }
           if (state is ResetpasswordSuccess) {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text(state.msg,
-                    style: const TextStyle(color: Colors.white))));
+            AppConst.showMessage(msg: state.msg, context: context);
             Navigator.pushNamed(context, Routes.loginRoute);
           } else if (state is ResetpasswordFailure) {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text(state.errorMessage,
-                    style: const TextStyle(color: Colors.white))));
+            AppConst.showMessage(msg: state.errorMessage, context: context);
+            isLoading = false;
           }
         },
         builder: (context, state) {
-          if (state is ResetpasswordLoading) {
-            return Center(
-              child: CircularProgressIndicator(
-                color: AppColors.mainColor,
-              ),
-            );
-          }
-          return SingleChildScrollView(
-            child: Form(
-              key: formKey,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 30.h,
-                    ),
-                    CustomAppBar(
-                      title: getTranslated("ChangePassword", context)!,
-                    ),
-                    SizedBox(
-                      height: 40.h,
-                    ),
-                    Center(
-                      child: Image.asset(
-                        'assets/images/forgetpassword3.png',
-                        height: 228.h,
-                        width: 248.w,
+          return ModalProgressHUD(
+            color: AppColors.mainColor,
+            blur: 0.01,
+            inAsyncCall: isLoading,
+            child: SingleChildScrollView(
+              child: Form(
+                key: formKey,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 30.h,
                       ),
-                    ),
-                    SizedBox(
-                      height: 30.h,
-                    ),
-                    Center(
-                      child: AppWidgets.defultTextRegister(
-                        text: getTranslated("resetPassword", context)!,
+                      CustomAppBar(
+                        title: getTranslated("ChangePassword", context)!,
                       ),
-                    ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    CustomTextFormField(
-                      hintText: getTranslated("NewPassword", context)!,
-                      nameofController: newPasswordController,
-                      errorMassage: getTranslated("errorNewPassword", context)!,
-                      regEx: AppConst.passwordPattern,
-                      isvisble: false,
-                      keyBoredType: TextInputType.visiblePassword,
-                      prefixIcon: const Icon(IconlyLight.lock),
-                    ),
-                    SizedBox(
-                      height: 10.h,
-                    ),
-                    CustomTextFormField(
-                      hintText: getTranslated("ConfirmPassword", context)!,
-                      nameofController: confirmPasswordController,
-                      errorMassage: getTranslated("errorConfirm", context)!,
-                      regEx: AppConst.passwordPattern,
-                      isvisble: false,
-                      keyBoredType: TextInputType.visiblePassword,
-                      prefixIcon: const Icon(IconlyLight.lock),
-                    ),
-                    SizedBox(
-                      height: 87.h,
-                    ),
-                    Center(
-                        child: DefultButton(
-                            onPressed: () {
-                              final sharedPreferences =
-                                  getIt<SharedPreferences>();
-                              String? verificationCode = sharedPreferences
-                                  .getString('verification_code');
-                              String? phoneNumber =
-                                  sharedPreferences.getString('phone_number');
-                              if (formKey.currentState!.validate()) {
-                                {
-                                  BlocProvider.of<ResetpasswordCubit>(context)
-                                      .resetPassword(
-                                          newPassword:
-                                              newPasswordController.text,
-                                          confirmPassword:
-                                              confirmPasswordController.text,
-                                          verificationCode: verificationCode!,
-                                          phoneNumber: phoneNumber!);
+                      SizedBox(
+                        height: 40.h,
+                      ),
+                      Center(
+                        child: Image.asset(
+                          'assets/images/forgetpassword3.png',
+                          height: 228.h,
+                          width: 248.w,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Center(
+                        child: AppWidgets.defultTextRegister(
+                          text: getTranslated("resetPassword", context)!,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      CustomTextFormField(
+                        hintText: getTranslated("NewPassword", context)!,
+                        nameofController: newPasswordController,
+                        errorMassage:
+                            getTranslated("errorNewPassword", context)!,
+                        regEx: AppConst.passwordPattern,
+                        isvisble: false,
+                        keyBoredType: TextInputType.visiblePassword,
+                        prefixIcon: const Icon(IconlyLight.lock),
+                      ),
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      CustomTextFormField(
+                        hintText: getTranslated("ConfirmPassword", context)!,
+                        nameofController: confirmPasswordController,
+                        errorMassage: getTranslated("errorConfirm", context)!,
+                        regEx: AppConst.passwordPattern,
+                        isvisble: false,
+                        keyBoredType: TextInputType.visiblePassword,
+                        prefixIcon: const Icon(IconlyLight.lock),
+                      ),
+                      SizedBox(
+                        height: 87.h,
+                      ),
+                      Center(
+                          child: DefultButton(
+                              onPressed: () {
+                                final sharedPreferences =
+                                    getIt<SharedPreferences>();
+                                String? verificationCode = sharedPreferences
+                                    .getString('verification_code');
+                                String? phoneNumber =
+                                    sharedPreferences.getString('phone_number');
+                                if (formKey.currentState!.validate()) {
+                                  {
+                                    BlocProvider.of<ResetpasswordCubit>(context)
+                                        .resetPassword(
+                                            newPassword:
+                                                newPasswordController.text,
+                                            confirmPassword:
+                                                confirmPasswordController.text,
+                                            verificationCode: verificationCode!,
+                                            phoneNumber: phoneNumber!);
+                                  }
                                 }
-                              }
-                            },
-                            text: getTranslated("Send", context)!))
-                  ],
+                              },
+                              text: getTranslated("Send", context)!))
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/features/forgetPassword/presentation/screens/verification_screen.dart
+++ lib/features/forgetPassword/presentation/screens/verification_screen.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_verification_code/flutter_verification_code.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:qr_care/config/Localization/Constraine.dart';
 import 'package:qr_care/config/routes/app_routes.dart';
 import 'package:qr_care/core/Services/notifi_service.dart';
 import 'package:qr_care/core/app_color.dart';
+import 'package:qr_care/core/app_constant.dart';
 import 'package:qr_care/core/app_widgets.dart';
-import 'package:qr_care/features/forgetPassword/cubit/resetpassword_cubit.dart';
+
 import 'package:qr_care/features/forgetPassword/cubit/verificationpassword_cubit.dart';
 import 'package:qr_care/features/forgetPassword/presentation/widgets/custom_appbar.dart';
 import 'package:qr_care/features/forgetPassword/presentation/widgets/resend_code.dart';
@@ -41,116 +43,113 @@ class _VerificationScreenState extends State<VerificationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      body: SingleChildScrollView(
-        child:
-            BlocConsumer<VerificationpasswordCubit, VerificationpasswordState>(
-          listener: (context, state) {
-            if (state is VerificationpasswordSuccess) {
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content: Text(state.msg,
-                      style: const TextStyle(color: Colors.white))));
-              Navigator.pushNamed(context, Routes.changePasswordRoute);
-            } else if (state is VerificationpasswordFailure) {
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content: Text(state.errorMessage,
-                      style: const TextStyle(color: Colors.white))));
-            }
-          },
-          builder: (context, state) {
-            if (state is VerificationpasswordLoading) {
-              return Center(
-                child: CircularProgressIndicator(
-                  color: AppColors.mainColor,
-                ),
-              );
-            }
-            return Form(
-              key: formKey,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 30.h,
-                    ),
-                    CustomAppBar(
-                      title: getTranslated("Verification", context)!,
-                    ),
-                    SizedBox(
-                      height: 40.h,
-                    ),
-                    Center(
-                      child: Image.asset(
-                        'assets/images/forgetpassword2.png',
-                        height: 228.h,
-                        width: 248.w,
+      body: BlocConsumer<VerificationpasswordCubit, VerificationpasswordState>(
+        listener: (context, state) {
+          if (state is VerificationpasswordLoading) {
+            widget.isLoading = true;
+          }
+          if (state is VerificationpasswordSuccess) {
+            AppConst.showMessage(msg: state.msg, context: context);
+            Navigator.pushNamed(context, Routes.changePasswordRoute);
+          } else if (state is VerificationpasswordFailure) {
+            AppConst.showMessage(msg: state.errorMessage, context: context);
+            widget.isLoading = false;
+          }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+            color: AppColors.mainColor,
+            blur: 0.01,
+            inAsyncCall: widget.isLoading,
+            child: SingleChildScrollView(
+              child: Form(
+                key: formKey,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 30.h,
                       ),
-                    ),
-                    SizedBox(
-                      height: 30.h,
-                    ),
-                    Center(
-                      child: AppWidgets.defultTextRegister(
-                        text: getTranslated("checkDigit", context)!,
+                      CustomAppBar(
+                        title: getTranslated("Verification", context)!,
                       ),
-                    ),
-                    SizedBox(
-                      height: 25.h,
-                    ),
-                    Center(
-                      child: VerificationCode(
-                        fullBorder: true,
-                        textStyle:
-                            TextStyle(fontSize: 20.sp, color: Colors.black),
-                        keyboardType: TextInputType.number,
-                        underlineColor: const Color(0xff004854),
-                        fillColor: Colors.white,
-                        length: 4,
-                        cursorColor: AppColors.mainColor,
-                        onCompleted: (String value) {
-                          setState(() {
-                            code = value;
-                          });
-                        },
-                        onEditing: (bool value) {},
-                        // onEditing: (bool value) {
-                        //   setState(() {
-                        //     _onEditing = value;
-                        //   });
-                        //   if (!_onEditing) FocusScope.of(context).unfocus();
-                        // },
+                      SizedBox(
+                        height: 40.h,
                       ),
-                    ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    Center(
-                      child: ResendCodeWidget(
-                        onPressed: () {
-                          showNotifi();
-                        },
+                      Center(
+                        child: Image.asset(
+                          'assets/images/forgetpassword2.png',
+                          height: 228.h,
+                          width: 248.w,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 93.h,
-                    ),
-                    Center(
-                        child: DefultButton(
-                            onPressed: () {
-                              if (formKey.currentState!.validate()) {
-                                BlocProvider.of<VerificationpasswordCubit>(
-                                        context)
-                                    .verificationFun(code: code!);
-                              }
-                            },
-                            text: getTranslated("ConfirmPassword", context)!))
-                  ],
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Center(
+                        child: AppWidgets.defultTextRegister(
+                          text: getTranslated("checkDigit", context)!,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25.h,
+                      ),
+                      Center(
+                        child: VerificationCode(
+                          fullBorder: true,
+                          textStyle:
+                              TextStyle(fontSize: 20.sp, color: Colors.black),
+                          keyboardType: TextInputType.number,
+                          underlineColor: const Color(0xff004854),
+                          fillColor: Colors.white,
+                          length: 4,
+                          cursorColor: AppColors.mainColor,
+                          onCompleted: (String value) {
+                            setState(() {
+                              code = value;
+                            });
+                          },
+                          onEditing: (bool value) {},
+                          // onEditing: (bool value) {
+                          //   setState(() {
+                          //     _onEditing = value;
+                          //   });
+                          //   if (!_onEditing) FocusScope.of(context).unfocus();
+                          // },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Center(
+                        child: ResendCodeWidget(
+                          onPressed: () {
+                            showNotifi();
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 93.h,
+                      ),
+                      Center(
+                          child: DefultButton(
+                              onPressed: () {
+                                if (formKey.currentState!.validate()) {
+                                  BlocProvider.of<VerificationpasswordCubit>(
+                                          context)
+                                      .verificationFun(code: code!);
+                                }
+                              },
+                              text: getTranslated("ConfirmPassword", context)!))
+                    ],
+                  ),
                 ),
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
