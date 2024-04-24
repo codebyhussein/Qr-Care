8691c0ab6ab6959e76a4df792b54256d7a0ad957
hussein_salah <husseinsalah2528@gmail.com>
2024-04-25 00:18:47 +0200

    just to test

--- lib/core/Services/Api/api_services.dart
+++ lib/core/Services/Api/api_services.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:qr_care/core/Services/Api/app_url.dart';
 
 class ApiService {
   static Future<http.MultipartRequest> postData(
@@ -16,5 +17,17 @@ class ApiService {
     var response =
         await http.post(Uri.parse(endPoint), body: body, headers: header);
     return response;
+  }
+
+  Future<List<dynamic>> getData({required String qr_data}) async {
+    final response = await http.get(Uri.parse(
+        'http://192.168.1.17/grd/medical_information.php?user_id=$qr_data'));
+
+    if (response.statusCode == 200) {
+      print(json.decode(response.body));
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load data');
+    }
   }
 }
